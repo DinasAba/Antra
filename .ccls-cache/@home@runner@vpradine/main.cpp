99b059c3b7f38b4d2dd641202c09b4babc0498f3
DinasAba <144428178+DinasAba@users.noreply.github.com>
2023-09-27 14:10:37 +0000

    Pridetas pasirinkimas (duomenys is failo/ duomenys is dokumento)

--- .ccls-cache/@home@runner@vpradine/main.cpp
+++ .ccls-cache/@home@runner@vpradine/main.cpp
@@ -4,7 +4,8 @@
 #include <iomanip>
 #include <algorithm>
 #include <cstdlib>
-
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
@@ -50,35 +51,68 @@ double skaiciuotiGalutiniBalaMediana(const vector <int>& pazymiai) {
 }
 
 int main() {
-  srand(static_cast<unsigned>(time(0)));
-  
-  int studentuSkaicius;
-  cout << "Iveskite studentu skaiciu:";
-  cin >> studentuSkaicius;
+    srand(static_cast<unsigned>(time(0)));
+    cout << "Pasirinkite duomenis (Iš dokumento - D, Iš įvedimo - I): ";
+    char duomenusaltinis;
+    cin >> duomenusaltinis;
+    vector<Studentas> studentai;
+    if (duomenusaltinis == 'I' || duomenusaltinis == 'i') {
+      int studentuSkaicius;
+      cout << "Iveskite studentu skaiciu:";
+      cin >> studentuSkaicius;
 
-  vector<Studentas> studentai;
-
-  for(int i = 0; i < studentuSkaicius; ++i) {
-    Studentas studentas;
-    cout << "Iveskite studento varda: ";
-    cin >> studentas.vardas;
-    cout << "Iveskite studento pavarde: ";
-    cin >> studentas.pavarde;
-    studentas.egzas = rand() % 10 + 1;
-    studentas.pazymiai = PazymiuGeneracija(rand() %10 + 1);
-    cout << "Pasirinkite skaiciavimo metoda (Vidurkis - V, Mediana - M): ";
-    char skaiciavimoMetodas;
-    cin >> skaiciavimoMetodas;
-    if (skaiciavimoMetodas == 'V' || skaiciavimoMetodas == 'v') {
-      studentas.galutinis = 0.4 * apskaiciuotiGalutiniBalaVidurkis(studentas.pazymiai, studentas.egzas) + 0.6 * studentas.egzas;
-    } else if (skaiciavimoMetodas == 'M' || skaiciavimoMetodas == 'm') {
-      studentas.galutinis = 0.4 * skaiciuotiGalutiniBalaMediana(studentas.pazymiai) + 0.6 * studentas.egzas;
-    } else {
-      cout << "Neteisingai parinktas skaiciavimo metodas. Taikomas vidurkis skaiciavimas" << endl;
-      studentas.galutinis = 0.4 * apskaiciuotiGalutiniBalaVidurkis(studentas.pazymiai, studentas.egzas) + 0.6 * studentas.egzas;
+      for(int i = 0; i < studentuSkaicius; ++i) {
+      Studentas studentas;
+      cout << "Iveskite studento varda: ";
+      cin >> studentas.vardas;
+      cout << "Iveskite studento pavarde: ";
+      cin >> studentas.pavarde;
+      studentas.egzas = rand() % 10 + 1;
+      studentas.pazymiai = PazymiuGeneracija(rand() %10 + 1);
+      cout << "Pasirinkite skaiciavimo metoda (Vidurkis - V, Mediana - M): ";
+      char skaiciavimoMetodas;
+      cin >> skaiciavimoMetodas;
+      if (skaiciavimoMetodas == 'V' || skaiciavimoMetodas == 'v') {
+        studentas.galutinis = 0.4 * apskaiciuotiGalutiniBalaVidurkis(studentas.pazymiai, studentas.egzas) + 0.6 * studentas.egzas;
+      } 
+      else if (skaiciavimoMetodas == 'M' || skaiciavimoMetodas == 'm') {
+        studentas.galutinis = 0.4 * skaiciuotiGalutiniBalaMediana(studentas.pazymiai) + 0.6 * studentas.egzas;
+      } 
+      else {
+        cout << "Neteisingai parinktas skaiciavimo metodas. Taikomas vidurkis skaiciavimas" << endl;
+        studentas.galutinis = 0.4 * apskaiciuotiGalutiniBalaVidurkis(studentas.pazymiai, studentas.egzas) + 0.6 * studentas.egzas;
+      }
+      studentai.push_back(studentas);
+      }
     }
-    studentai.push_back(studentas);
-  }
+      
+    else if (duomenusaltinis == 'D' || duomenusaltinis == 'd') {
+      ifstream inputFile("kursiokai.txt");
+      if (!inputFile.is_open()) {
+      cerr << "Klaid - neatidaromas failas" << endl;
+      return 1;
+      }
+  
+      string line;
+      
+      while (getline(inputFile, line)) {
+        istringstream iss(line);
+        Studentas studentas;
+        iss >> studentas.pavarde >> studentas.vardas;
+        studentas.pazymiai.clear();
+        int pazymys;
+        while (iss >> pazymys) {
+            studentas.pazymiai.push_back(pazymys);
+        }
+        studentas.egzas = studentas.pazymiai.back();
+        studentas.pazymiai.pop_back();
+        studentai.push_back(studentas);
+      }
+    
+      inputFile.close();
+      
+    }
+  
 
   cout << "Pavarde\tVardas\tGalutinis(Vid)\tGalutinis(Med)" << endl;
   cout << "---------------------------------------------" << endl;
