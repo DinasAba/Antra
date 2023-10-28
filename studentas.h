a0d05a6ca8d5a80d305ef7b9102b216ad599075f
DinasAba <144428178+DinasAba@users.noreply.github.com>
2023-10-29 00:54:23 +0300

    Add files via upload

--- studentas.h
+++ studentas.h
@@ -3,16 +3,19 @@
 
 #include <string>
 #include <vector>
+#include <list>
 
 struct Studentas {
     std::string vardas;
     std::string pavarde;
-    std::vector<int> pazymiai;
+    std::list<int> pazymiai;
     int egzas;
     double galutinis_vidurkis;
+    double galutinis_mediana;
 };
 
-double apskaiciuotiGalutiniBalaVidurkis(const std::vector<int>& pazymiai, int egzas);
+double apskaiciuotiGalutiniBalaVidurkis(const std::list<int>& pazymiai, int egzas);
 bool RusiavimasPavarde(const Studentas& a, const Studentas& b);
-
+std::list<int> PazymiuGeneracija(int kiekis);
+double skaiciuotiGalutiniBalaMediana(const std::list<int>& pazymiai);
 
